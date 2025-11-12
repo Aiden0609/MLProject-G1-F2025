fefae079926a978c3edc1256e97290a08b1083cd
Bogadisa <magnus@hoddevik.eu>
2025-11-12 11:59:51 -0330

    Testing data from microsoft

---
+++ testing.py
@@ -0,0 +1,20 @@
+import pickle
+import numpy as np
+from huggingface_hub import hf_hub_download
+
+# Download the static variables from HuggingFace.
+# static_path = hf_hub_download(
+#     repo_id="microsoft/aurora",
+#     filename="aurora-0.25-wave-static.pickle",
+# )
+static_path = hf_hub_download(
+    repo_id="microsoft/aurora",
+    filename="aurora-0.4-air-pollution-static.pickle",
+)
+print("Static variables downloaded!")
+
+with open(static_path, "rb") as f:
+    static_vars: dict[str, np.ndarray] = pickle.load(f)
+
+for var, value in static_vars.items():
+    print(f"shape of {var=} {value.shape}")
